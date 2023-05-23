822bf1bd973d014ee979a63b6709bfa1ef9e97d7
DustyCube <113673336+Dusty-Cube@users.noreply.github.com>
2023-05-23 23:27:46 +0600

    Uploaded task 8

---
+++ Module 4/PHP Part - 8/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$array = [
+    'name1' => 'Nasim',
+    'name2' => 'Rahaman',
+    'num' => 28,
+    'float' => 23.22
+];
+
+echo $array['name1'], '<br/>';
+echo $array['name2'], '<br/>';
+echo $array['num'], '<br/>';
+echo $array['float'], '<br/>';
